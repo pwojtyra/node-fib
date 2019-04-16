a89b215d5ecace62323674db166dee6072555303
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 22:03:14 +0200

    change code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 var fib = function(n, callback) {
 
@@ -22,7 +22,6 @@ app.get('/:n', function(req, res) {
     });
 });
 
-// app.listen(server_port, server_ip_address, function () {
-app.listen(server_port, function () {
-    console.log( "Listening on port " + server_port )
+app.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
   });
