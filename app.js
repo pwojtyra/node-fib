ae9ab4ca70d1a305982c698dd73b5eac47b36be9
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 16:33:39 +0200

    correct var spelling

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = require('express').createServer();
 var fib = function(n, callback) {
 
     const fibonacci = require ('fibonacci');
-    const binNumber = fibonacci.iterate (n);
-    callback (null, binNumber)
+    const bigNumber = fibonacci.iterate (n);
+    callback (null, bigNumber);
     return;
 }
 
 app.get('/:n', function(req, res) {
-    
+
     fib(req.params.n, function(err, number) { 
         res.send(''+number);
     });
