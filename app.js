241d1b559f6aca1b50977b6f14b55881b1a3a8b1
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 21:49:54 +0200

    return to express

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '172.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 var fib = function(n, callback) {
-  console.log (n);
-  const fibonacci = require ('fibonacci');
-  const bigNumber = fibonacci.iterate (n);
-  console.log (bigNumber);
-  callback (bigNumber);
-  return;
+
+    console.log (n);
+    const fibonacci = require ('fibonacci');
+    const bigNumber = fibonacci.iterate (n);
+    console.log (bigNumber);
+    callback (bigNumber);
+    return;
 }
 
-http.createServer(function (req, res) {
-  fib(req.params.n, function(number) {
-    console.log (number);
-    res.end(number);
+app.get('/:n', function(req, res) {
+
+    fib(req.params.n, function(number) {
+        console.log (number);
+        res.send(number);
     });
-  }).listen(8080);
+});
+
+app.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+  });
