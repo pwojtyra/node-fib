22c963e7c4921deb505c09805e9cbf2aae362d58
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 21:47:00 +0200

    change code

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ var http = require('http');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '172.0.0.1'
 
-var fib = function(n) {
-
-    console.log (n);
-    const fibonacci = require ('fibonacci');
-    const bigNumber = fibonacci.iterate (n);
-    console.log (bigNumber);
-    return(bigNumber);
+var fib = function(n, callback) {
+  console.log (n);
+  const fibonacci = require ('fibonacci');
+  const bigNumber = fibonacci.iterate (n);
+  console.log (bigNumber);
+  callback (bigNumber);
+  return;
 }
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    console.log (q.n);
-    var number = fib(q.n);
+  fib(req.params.n, function(number) {
     console.log (number);
     res.end(number);
-
+    });
   }).listen(8080);
