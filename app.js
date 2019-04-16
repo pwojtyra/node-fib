1ccb2d4a88dc2e7c385793193371297d9104ca4c
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 22:36:16 +0200

    update code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
+//var fs = require('fs');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
@@ -16,7 +16,6 @@ var fib = function(n, callback) {
 }
 
 app.get('/:n', function(req, res) {
-
     fib(req.params.n, function(number) {
         console.log (number);
         res.send(number);
