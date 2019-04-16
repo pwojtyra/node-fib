21a4184c4fd41cf37b7294334f65b3512c5986eb
Pawel <pwojtyra@i-s.com.pl>
2019-04-16 22:40:27 +0200

    change code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
